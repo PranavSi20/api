d5f824625465edcc0e940111ad5a47f58dfb78b6
PranavSi20 <pranavkumarsoni.si2@iacademia.in>
2020-04-29 16:24:59 +0530

    third1

--- models/user.js
+++ models/user.js
@@ -8,6 +8,9 @@ var UserSchema = new Schema({
         unique: true,
         match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
     },
+     password: {
+        type: String, required: true
+     },
   name: {
     type: String,
     required: true
