390a521b579217dc4a2e28418d7d382ce12a0019
PranavSi20 <pranavkumarsoni.si2@iacademia.in>
2020-05-08 19:45:46 +0530

    submit route

--- routes/grievances.js
+++ routes/grievances.js
@@ -2,11 +2,27 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 
-const User = require("../models/grievances");
+const Grievance = require("../models/grievances");
 
-router.post('/submit', function(req,res){
-    const grievances = new Grievance
-});
+router.post("/submit", (req, res, next) => {
+    const grievances = new Grievance({
+      _id: new mongoose.Types.ObjectId(),
+      gid: req.body.gid,
+      wid: req.body.wid,
+      eligibleForMeeting: req.body.eligibleForMeeting,
+      time: req.body.time,
+      date: req.body.date,
+      address: req.body.address,
+      summary: req.body.summary,
+      review: req.body.review,
+      status: req.body.status
+      
+    });
+    user.save(function(err, user) {
+      if (err) return console.error(err);
+      console.log(" saved to grievance collection.");
+    });
+  });
 
 
 
