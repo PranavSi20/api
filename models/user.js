c4aba780723e18b384bdd5c756ecfae696238baf
gouravp2007 <gouravpradhan.si20@iacademia.in>
2020-06-09 18:48:12 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -115,8 +115,8 @@ validateDOB(){
     required: true,
     match:"^[2-9]{1}[0-9]{3}\\s[0-9]{4}\\s[0-9]{4}$"
   },
-}
-);
+
+});
 
 module.exports = mongoose.model('user', UserSchema);
 module.exports = function(user) {
